5a331ccb3a03632411b867bb7c9db8b82035c4fa
MarkBebrov <92856826+MarkBebrov@users.noreply.github.com>
2023-06-03 21:51:36 +0300

    WOW

--- train_finetune.py
+++ train_finetune.py
@@ -11,37 +11,29 @@ if __name__ == '__main__':
     import multiprocessing
     multiprocessing.freeze_support()
 
-    # Загрузите предобученную модель
+    
     model = torch.load('C:/BIRDFINAL/trained_model.pth')
 
-    # Измените последний слой модели
-    num_classes = 525  # Замените на количество классов в вашем датасете
+    num_classes = 525  
     model.fc = nn.Linear(model.fc.in_features, num_classes)
 
-    # Получите данные для обучения
     dataloaders, dataset_sizes, _ = load_data()
 
-    # Определите функцию потерь и оптимизатор
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-    # Выполните дообучение модели
     model = train_model(model, criterion, optimizer, exp_lr_scheduler, dataloaders, dataset_sizes, num_epochs=30)
 
-    # Сохраните дообученную модель
     torch.save(model, 'C:/BIRDFINAL/finetuned_model.pth')
 
-    # Получите статистику обучения и валидации
     train_loss = model.train_loss
     valid_loss = model.valid_loss
     train_acc = model.train_acc
     valid_acc = model.valid_acc
 
-    # Постройте графики потерь и точности
     epochs = range(1, len(train_loss) + 1)
 
-    # График потерь
     plt.figure()
     plt.plot(epochs, train_loss, 'r', label='Training loss')
     plt.plot(epochs, valid_loss, 'b', label='Validation loss')
@@ -50,7 +42,6 @@ if __name__ == '__main__':
     plt.ylabel('Loss')
     plt.legend()
 
-    # График точности
     plt.figure()
     plt.plot(epochs, train_acc, 'r', label='Training accuracy')
     plt.plot(epochs, valid_acc, 'b', label='Validation accuracy')
